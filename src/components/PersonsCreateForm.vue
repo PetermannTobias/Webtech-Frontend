11ed35315ea224ce4ac0582c50a82f6bb65c8ecc
PetermannTobias <saibot.96@gmx.net>
2022-06-15 18:09:35 +0200

    Create Frontend hinzugefügt noch Fehler

--- src/components/PersonsCreateForm.vue
+++ src/components/PersonsCreateForm.vue
@@ -8,14 +8,74 @@
       <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <p>The body</p>
+      <form class="needs-validation text-start" novalidate>
+        <div class="mb-3">
+          <label for="first-name" class="form-label">First name</label>
+          <input type="text" class="form-control" id="first-name" v-model="firstName" required>
+        </div>
+        <div class="mb-3">
+          <label for="last-name" class="form-label">Last name</label>
+          <input type="text" class="form-control" id="last-name" v-model="lastName" required>
+        </div>
+        <div class="mb-3">
+          <label for="gender" class="form-label">Gender</label>
+          <select id="gender" class="form-select" v-model="gender">
+            <option value="" selected>Choose...</option>
+            <option value="MALE">Male</option>
+            <option value="FEMALE">Female</option>
+            <option value="DIVERSE">Diverse</option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="vaccinated" v-model="vaccinated">
+            <label class="form-check-label" for="vaccinated">
+              Vaccinated
+            </label>
+          </div>
+        </div>
+        <div class="mt-5">
+          <button class="btn btn-primary me-3" type="submit" @click="createPerson">Create</button>
+          <button class="btn btn-danger" type="reset">Reset</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PersonsCreateForm'
+  name: 'PersonsCreateForm',
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      gender: '',
+      vaccinated: false
+    }
+  },
+  methods: {
+    createPerson () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/persons'
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+      const payload = JSON.stringify({
+        firstName: this.firstName,
+        lastName: this.lastName,
+        vaccinated: this.vaccinated,
+        gender: this.gender
+      })
+      const requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: payload,
+        redirect: 'follow'
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.text())
+        .catch(error => console.log('error', error))
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView'
-import Persons from '@/views/Persons'
+import AboutView from '../views/AboutView.vue'
+import Persons from '@/views/Persons.vue'
 
 const routes = [
   {
